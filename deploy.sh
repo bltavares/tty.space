bc3b7332b934f2c9fd55a3b8cc493ee1dcb1998b
Bruno Tavares <bruno.exz@gmail.com>
2015-04-25 10:55:26 -0300

    Setup a deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if ! which kickstart > /dev/null; then
+  echo kickstart not found on the PATH.
+  echo Check it on https://github.com/bltavares/kickstart
+  exit 1
+fi
+
+kickstart deploy root@tty.space users
