599deb53519b4194a475f0565f9e7c52f7baafca
Bruno Tavares <bruno.exz@gmail.com>
2015-04-25 13:49:58 -0300

    Sets up a simple index page

--- roles/web.sh
+++ roles/web.sh
@@ -5,5 +5,8 @@ kickstart.package.install nginx
 
 cp files/nginx.conf /etc/nginx/nginx.conf
 
+rm -fr /var/web
+cp -r files/web /var/web
+
 kickstart.service.enable nginx
 kickstart.service.restart nginx
