f5ab948d9f6376f3f11c732cff2ea560a39c649b
Bruno Tavares <bruno.exz@gmail.com>
2015-05-07 12:10:22 -0300

    Check if the new_users.tmp has content

--- roles/mail.sh
+++ roles/mail.sh
@@ -8,7 +8,7 @@ newaliases
 kickstart.service.enable postfix
 kickstart.service.restart postfix
 
-if [ -f new_users.tmp ]; then
+if [ -s new_users.tmp ]; then
  mail -s "Welcome to tty.space" $(cat new_users.tmp) < files/welcome.mail
  rm new_users.tmp
 fi
