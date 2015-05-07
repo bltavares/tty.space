3d75f5afb9d56fef4ca26453acc722b43f20c911
Bruno Tavares <bruno.exz@gmail.com>
2015-05-07 12:12:37 -0300

    Adds information that it is sending out welcome emails

--- roles/mail.sh
+++ roles/mail.sh
@@ -9,6 +9,7 @@ kickstart.service.enable postfix
 kickstart.service.restart postfix
 
 if [ -s new_users.tmp ]; then
+ kickstart.info "Sending welcome email for new users"
  mail -s "Welcome to tty.space" $(cat new_users.tmp) < files/welcome.mail
  rm new_users.tmp
 fi
