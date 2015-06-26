e5c2f093c61c80ca145cd71adc484393209db85c
Bruno Tavares <bruno.exz@gmail.com>
2015-06-26 12:03:30 -0300

    Alpine fails to compile - need to look deeper

--- roles/applications.sh
+++ roles/applications.sh
@@ -2,6 +2,3 @@ kickstart.context 'applications'
 
 kickstart.package.install vim
 kickstart.package.install mutt
-
-source recipes/aur.sh
-kickstart.package.installed alpine || nobody.packer -S --noconfirm alpine
