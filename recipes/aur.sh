5a55c35cd4dad59680d6f33b9bb4022098867e48
Bruno Tavares <bruno.exz@gmail.com>
2015-08-28 14:05:10 -0300

    Updates dependencies installations for packer

--- recipes/aur.sh
+++ recipes/aur.sh
@@ -1,9 +1,13 @@
 install.packer() {
   kickstart.info 'setting up packer'
   rm -rf /tmp/packer
-  (cd /tmp && curl https://aur.archlinux.org/packages/pa/packer/packer.tar.gz | tar xz)
+  (cd /tmp && curl https://aur4.archlinux.org/cgit/aur.git/snapshot/packer.tar.gz | tar xz)
   chown -R nobody:nobody /tmp/packer
-  kickstart.package.install jshon expac
+  kickstart.package.install jshon
+  kickstart.package.install expac
+  kickstart.package.install sudo
+  kickstart.package.install base-devel
+  kickstart.package.install git
   kickstart.user.exec nobody 'cd /tmp/packer && makepkg'
   pacman --noconfirm -U /tmp/packer/packer*.pkg.tar.xz
 }
