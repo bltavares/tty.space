991dd8c52f0c7b5580af3966e627b4a603042756
Bruno Tavares <connect+github@bltavares.com>
2015-11-24 00:48:05 -0200

    Configures the ssl options to a stronger set
    Configured accordingly to https://weakdh.org/sysadmin.html

--- roles/web.sh
+++ roles/web.sh
@@ -6,6 +6,7 @@ kickstart.package.update
 kickstart.package.install nginx
 
 cp files/nginx.conf /etc/nginx/nginx.conf
+! [ -f /etc/nginx/dhparams.pem ] && openssl dhparam -out /etc/nginx/dhparams.pem 2048
 
 mkdir -p /var/web
 userlist="$(web.userList)" \
