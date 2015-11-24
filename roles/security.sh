542de517ae8a44d7bec07b708657c9c331680430
Bruno Tavares <connect+github@bltavares.com>
2015-11-24 00:49:57 -0200

    Enables ports over ipv6
    Configured accordingly to
    http://linuxcommand.org/man_pages/ip6tables8.html and
    http://www.toofishes.net/blog/making-things-ipv6-capable/

--- roles/security.sh
+++ roles/security.sh
@@ -25,3 +25,22 @@ iptables -P INPUT DROP
 iptables-save > /etc/iptables/iptables.rules
 kickstart.service.restart iptables
 kickstart.service.enable iptables
+
+ip6tables -F
+ip6tables -N sshguard || true
+ip6tables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+ip6tables -A INPUT -j sshguard
+ip6tables -A INPUT -i lo -j ACCEPT
+ip6tables -A INPUT -p tcp --dport 22 -j ACCEPT
+ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT
+ip6tables -A INPUT -p tcp --dport 443 -j ACCEPT
+ip6tables -A INPUT -m conntrack --ctstate INVALID -j DROP
+ip6tables -A INPUT -p ipv6-icmp --icmpv6-type 8 -m conntrack --ctstate NEW -j ACCEPT
+ip6tables -A INPUT -p udp -j REJECT --reject-with icmp6-port-unreachable
+ip6tables -A INPUT -p tcp -j REJECT --reject-with tcp-reset
+ip6tables -A INPUT -j REJECT
+ip6tables -P INPUT DROP
+ip6tables-save > /etc/iptables/ip6tables.rules
+
+kickstart.service.restart ip6tables
+kickstart.service.enable ip6tables
