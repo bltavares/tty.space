45efa4f547a3b1bb61047eab1eb6d0486ba1eba8
Bruno Tavares <connect+github@bltavares.com>
2017-05-20 18:02:53 -0300

    Migrating to Travis automatic deployments

--- roles/mail.sh
+++ roles/mail.sh
@@ -10,10 +10,10 @@ kickstart.service.restart postfix
 
 
 if [ -s new_users.tmp ]; then
- kickstart.info "Sending welcome email for new users"
- for f in files/emails/*; do
-     title=$(basename "$f")
-    mail -s "$title" $(cat new_users.tmp) < "$f"
- done
- rm new_users.tmp
+    kickstart.info "Sending welcome email for new users"
+    for f in files/emails/*; do
+        title=$(basename "$f")
+        mail -s "$title" $(cat new_users.tmp) < "$f"
+    done
+    rm new_users.tmp
 fi
